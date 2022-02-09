cea7375ba6eaebd6aa3fdcd280c7b64d5beabf0b
Tariq Ali <tariqali@Tariqs-MacBook-Pro.local>
2022-02-09 13:02:39 +0300

    admi

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: 'register',
+        name: 'registerAdmin',
         exec_mode: 'cluster',
         instances: 1, // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
